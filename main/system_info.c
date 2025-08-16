44391e6469e43b9e8186703a4b2aac15ecce3d59
zy <2774187105@qq.com>
2025-08-16 10:17:49 +0800

    system_info_demo

---
+++ main/system_info.c
@@ -0,0 +1,6 @@
+#include "system_info.h"
+#include <string.h>
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_log.h"
+#include "esp_system.h"
---
+++ main/system_info.h
@@ -0,0 +1,18 @@
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+#include "esp_err.h"
+
+
+//初始化系统信息模块,初始化NVS并从Flash中加载配置信息
+void system_info_init(void);
+
+//设置SN
+esp_err_t system_info_set_sn(const char* sn);
+
+//读取SN
+const char* system_info_get_sn(void);
+
+//命令处理test_device_get_sn,test_device_set_sn=...
+void system_info_cmd_handler(const char *command, const char *args);
+
+#endif // SYSTEM_INFO_H
